f98d176637c6b8ea1255570677889240e5a6c80e
Mederbek Burkanov <mederburkanov@gmail.com>
2022-03-19 03:03:58 +0600

    Initial Commit

---
+++ RevolteChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RevolteChat
+//
+//  Created by Mederbek on 19/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
