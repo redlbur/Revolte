0c31deaec09f585aa585e66bc972bcdc64a7e50e
Mederbek Burkanov <mederburkanov@gmail.com>
2022-03-19 10:08:07 +0600

    animation

--- RevolteChat/Controllers/ChatViewController.swift
+++ RevolteChat/Controllers/ChatViewController.swift
@@ -9,5 +9,14 @@ import Foundation
 import UIKit
 
 class ChatViewController: UIViewController{
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
     
 }
--- RevolteChat/Controllers/WelcomeViewController.swift
+++ RevolteChat/Controllers/WelcomeViewController.swift
@@ -14,9 +14,18 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleLable.text = ""
+        
+        var charIndex = 0.0
+        
+        let titleText = "REVOLTE"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLable.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
-
-
 }
 
